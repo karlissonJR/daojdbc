93da6390b3b08045fa66cbde27c8fd7f1bf305f7
karlissonJR <karlissonj@gmail.com>
2022-09-14 10:19:31 -0300

    Entidade vendedor(a) criada

--- src/application/App.java
+++ src/application/App.java
@@ -2,14 +2,20 @@ package application;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.Connection;
+import java.time.LocalDate;
 
 public class App {
 
     public static void main(String[] args) {
         Department obj = new Department(1, "Livros");
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
+
         System.out.println(obj);
+        System.out.println(seller);
     }
 
 }
