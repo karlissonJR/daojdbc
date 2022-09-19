a6d72f2480a17e24265f60e350fcb4f249d6e401
karlissonJR <karlissonj@gmail.com>
2022-09-19 08:35:14 -0300

    updateDAO

--- src/application/App.java
+++ src/application/App.java
@@ -28,12 +28,21 @@ public class App {
 //        List<Seller> sellersByDepartment = sellerDao.findByDepartment(department);
 //        sellersByDepartment.forEach(System.out::println);
 
-        List<Seller> allSellers = sellerDao.findAll();
-        allSellers.forEach(System.out::println);
+//        List<Seller> allSellers = sellerDao.findAll();
+//        allSellers.forEach(System.out::println);
 
 //        Seller seller = new Seller("Greg", "greg@gmail.com", birthDate, 4000.0, department);
 //        sellerDao.insert(seller);
 //        System.out.println(seller);
+
+
+        Seller seller = sellerDao.findById(11);
+
+        seller.setName("Orejuela");
+        seller.setEmail("carlpp1@gmail.com");
+        sellerDao.update(seller);
+        seller = sellerDao.findById(11);
+        System.out.println(seller);
     }
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -63,6 +63,28 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public void update(Seller obj) {
 
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(
+                    "UPDATE seller SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? WHERE Id = ?"
+            );
+
+            preparedStatement.setString(1, obj.getName());
+            preparedStatement.setString(2, obj.getEmail());
+            preparedStatement.setDate(3, Date.valueOf(obj.getBirthDate()));
+            preparedStatement.setDouble(4, obj.getBaseSalary());
+            preparedStatement.setInt(5, obj.getDepartment().getId());
+            preparedStatement.setInt(6, obj.getId());
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(preparedStatement);
+        }
+
     }
 
     @Override
